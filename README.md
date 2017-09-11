c7cf14e5761be3355654f52b33f485e94edd3fd3
itsuresh81 <itsuresh81@gmail.com>
2017-09-11 19:45:07 -0400

    Adding files to repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# Suresh Muralidharan
+
+- Project Name: Unix Workbench Assignment
+The make file was created on Monday 2017-09-11 19:37:27
+- The code contains 33 guessinggame.sh lines
---
+++ guessinggame.sh
@@ -0,0 +1,33 @@
+echo "Enter a directory where you want to count the no. of files (Ex. /home/valid_dir_name) :"
+read path
+
+if [ -d $path ]
+then
+	cd $path
+
+echo "\nNow lets play a game !! Guess the no. of files in the directory ?"
+read resp
+i=1
+
+file_cnt=`eval ls -l | wc -l`
+
+while [ $file_cnt -ne $resp ]
+do
+	if [ $file_cnt -le $resp ]
+	then
+		echo "Your guess is too high. Enter another number "
+		read resp
+		i=`expr $i + 1`
+	else
+		echo "Your guess is too low. Enter another number"
+		read resp
+		i=`expr $i + 1`
+	fi
+done
+
+echo "\nCongratulations !! You guessed it right on $i attempts\n"
+
+else
+	echo "\nDirectory not found. Please enter a valid directory\n"
+
+fi
